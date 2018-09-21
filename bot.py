34b9b3185b6c6393fbe0e220bb9b4bab093e55a9
zhiyue.wang <zhiyue.wang@rakuten.com>
2018-09-21 11:11:59 +0900

    bugfix

--- bot.py
+++ bot.py
@@ -18,16 +18,14 @@ ERR_NO_PERMISSION="请先使用/su取得系统权限！"
 
 def user_auth(telegram_id):
     global rootid, userid
-    if str(telegram_id) == str(rootid) or str(telegram_id) is in str(userid):
+    if str(telegram_id) == str(rootid) or str(telegram_id) in userid:
         return True
     else:
         return False
 
 def xb(bot, update):
     logging.info(str(update.message.chat_id) + " send /xb")
-    if  user_auth(update.message.chat_id):
-        bot.send_message(chat_id=update.message.chat_id, text=ERR_NO_PERMISSION)
-    else:
+    if user_auth(update.message.chat_id):
         os.system('rm '+xiaobo_url)
         os.system('kill -9 `ps ax | grep [x]iaobo | sed \'s/^\s*//\' | cut -d " " -f 1` && sleep 5')
         os.system('tmux new-window -n xiaobo "cd ~/workspaces/xiaoboQQBot/src && python xiaobo.py"')
@@ -36,9 +34,7 @@ def xb(bot, update):
 
 def qr(bot, update):
     logging.info(str(update.message.chat_id) + " send /qr")
-    if  user_auth(update.message.chat_id):
-        bot.send_message(chat_id=update.message.chat_id, text=ERR_NO_PERMISSION)
-    else:
+    if user_auth(update.message.chat_id):
         try:
             xiaobo_qr = open(xiaobo_url, 'rb')
             bot.send_photo(chat_id=update.message.chat_id, photo=xiaobo_qr)
@@ -48,12 +44,12 @@ def qr(bot, update):
             bot.send_message(chat_id=update.message.chat_id, text="没有二维码文件，请先/xb")
         else:
             bot.send_message(chat_id=update.message.chat_id, text="小波扫描二维码")
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text=ERR_NO_PERMISSION)
 
 def sts(bot, update):
     logging.info(str(update.message.chat_id) + " send /sts")
-    if  True:#user_auth(update.message.chat_id):
-        bot.send_message(chat_id=update.message.chat_id, text='这个功能还没写好！')#ERR_NO_PERMISSION)
-    else:
+    if False:#user_auth(update.message.chat_id):
         try:
             xiaobo_qr = open(xiaobo_url, 'rb')
             bot.send_photo(chat_id=update.message.chat_id, photo=xiaobo_qr)
@@ -63,16 +59,18 @@ def sts(bot, update):
             bot.send_message(chat_id=update.message.chat_id, text="没有二维码文件，请先/xb")
         else:
             bot.send_message(chat_id=update.message.chat_id, text="小波扫描二维码")
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text=ERR_NO_PERMISSION)
 
 def cc(bot, update):
     logging.info(str(update.message.chat_id) + " send /cc")
-    if  user_auth(update.message.chat_id):
-        bot.send_message(chat_id=update.message.chat_id, text=ERR_NO_PERMISSION)
-    else:
+    if user_auth(update.message.chat_id):
         os.system('rm ~/workspaces/xiaoboQQBot/src/cookie/*')
         os.system('rm ~/workspaces/xiaoboQQBot/src/smart_qq_bot/__pycache__/*')
         os.system('rm ~/workspaces/xiaoboQQBot/src/smart_qq_plugins/__pycache__/*')
         bot.send_message(chat_id=update.message.chat_id, text="小波的缓存已清除！")
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text=ERR_NO_PERMISSION)
 
 def su(bot, update, args):
     logging.info(str(update.message.chat_id) + " send /su " + ' '.join(args))
@@ -104,7 +102,9 @@ class myThread1(threading.Thread):
                 logout = datetime.datetime.now()
                 online_status='小波已掉线！\n掉线时间: '+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
                 logging.info(online_status)
-                bot.send_message(chat_id=userid, text=online_status)
+                for uid in userid:
+                    bot.send_message(chat_id=uid, text=online_status)
+                bot.send_message(chat_id=rootid, text=online_status)
                 alert = True
             elif pic_exist == False and alert == True:
                 login = datetime.datetime.now()
@@ -114,13 +114,15 @@ class myThread1(threading.Thread):
                 logout_hour   = str(int(logout_duration / 3600))
                 online_status='小波已恢复上线\n上线时间: '+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\n本次掉线: '+logout_hour+'小时'+logout_min+'分'+logout_sec+'秒'
                 logging.info(online_status)
-                bot.send_message(chat_id=userid, text=online_status)
+                for uid in userid:
+                    bot.send_message(chat_id=uid, text=online_status)
+                bot.send_message(chat_id=rootid, text=online_status)
                 alert = False
             sleep(5)
 
 if __name__ == '__main__':
     xb_handler = CommandHandler('xb', xb)
-    qq_handler = CommandHandler('qr', qr)
+    qr_handler = CommandHandler('qr', qr)
     sts_handler = CommandHandler('sts', sts)
     cc_handler = CommandHandler('cc', cc)
     su_handler = CommandHandler('su', su, pass_args=True)
@@ -140,9 +142,9 @@ if __name__ == '__main__':
         th1.start()
     except:
         logging.warning("无法启动小波管理台！")
-        bot.send_message(chat_id=userid, text="无法启动小波管理台！")
+        bot.send_message(chat_id=rootid, text="无法启动小波管理台！")
     else:
         logging.info("小波管理台已启动，输入任何消息查看使用帮助。")
-        bot.send_message(chat_id=userid, text="小波管理台已启动，输入任何消息查看使用帮助。")
+        bot.send_message(chat_id=rootid, text="小波管理台已启动，输入任何消息查看使用帮助。")
     finally:
         updater.start_polling()
